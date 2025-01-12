31cf569a7f7c90eea028afcdee4f2b12383ccc5d
Lucas Bonny <lucas@gunthercloud.com.br>
2025-01-12 08:48:26 -0300

    first commit

---
+++ src/main/java/br/com/gunthercloud/projectyt/ProjectytApplication.java
@@ -0,0 +1,13 @@
+package br.com.gunthercloud.projectyt;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProjectytApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProjectytApplication.class, args);
+	}
+
+}
